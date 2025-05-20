f0805eb9dd926a48373c055048f2204be19b0d67
Rofaida-Asem <107304774+Rofaida-Asem@users.noreply.github.com>
2025-05-20 13:45:53 +0300

    add add note cubit to add note bottom sheet

---
+++ lib/views/widgets/add_note_form.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widgets/custom_botton.dart';
+import 'package:notes_app/views/widgets/custom_text_field.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formKey = GlobalKey();
+
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CustomTextFormField(
+            onSaved: (value) {
+              title = value;
+            },
+            hintText: 'title',
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 16.0),
+            child: CustomTextFormField(
+              onSaved: (value) {
+                subTitle = value;
+              },
+              hintText: 'content',
+              maxLines: 5,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 40.0, bottom: 40),
+            child: CustomButton(
+              onTap: () {
+                if (formKey.currentState!.validate()) {
+                  formKey.currentState!.save();
+                } else {
+                  autovalidateMode = AutovalidateMode.always;
+                  setState(() {});
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
